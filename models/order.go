a2b501f90d6b196b3375352164abd8a310a5d115
RomansyahOfficial <ricky@hotelmurah.com>
2022-10-11 14:13:46 +0700

    Updated

--- models/order.go
+++ models/order.go
@@ -3,15 +3,26 @@ package models
 import (
 	"time"
 
+	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
 )
 
 type Order struct {
-	ID           int        `gorm:"primaryKey" json:"id"`
-	CustomerName string     `gorm:"not null;type:varchar(150)" json:"customer_name" valid:"required~customer name is required"`
-	OrderedAt    *time.Time `gorm:"autoCreateTime" json:"ordered_at"`
-	Items        []Item     `json:"items"`
-	CreatedAt    *time.Time
-	UpdatedAt    *time.Time
-	DeletedAt    gorm.DeletedAt
+	ID           int            `gorm:"primaryKey" json:"id"`
+	CustomerName string         `gorm:"not null;type:varchar(150)" json:"customer_name" valid:"required~customer name is required"`
+	OrderedAt    *time.Time     `gorm:"autoCreateTime" json:"ordered_at"`
+	Items        []Item         `json:"items,omitempty"`
+	CreatedAt    *time.Time     `json:"created_at,omitempty"`
+	UpdatedAt    *time.Time     `json:"updated_at,omitempty"`
+	DeletedAt    gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+}
+
+func (i *Order) BeforeCreate(tx *gorm.DB) (err error) {
+
+	_, errCreate := govalidator.ValidateStruct(i)
+	if errCreate != nil {
+		return errCreate
+	}
+
+	return
 }
